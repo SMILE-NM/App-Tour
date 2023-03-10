b5f105edce4b4359fbfac37344211b6896d8fd64
Nematov Muhammadjon <nematovmz2003@gmail.com>
2023-03-10 09:02:08 +0500

    version 2

---
+++ script.js
@@ -0,0 +1,8 @@
+function popShift(arr) {
+    let popped = arr.shift(); // Change this line
+    let shifted = arr.pop(); // Change this line
+    console.log(arr)
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete', 'Hello world']));
